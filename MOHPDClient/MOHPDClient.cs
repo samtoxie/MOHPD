7d2973df02e4b1237fe95046feeaf22b6afade93
Sam Toxopeus <sam@samtoxie.nl>
2020-05-07 22:50:12 +0200

    Implemented a panicbutton

--- MOHPDClient/MOHPDClient.cs
+++ MOHPDClient/MOHPDClient.cs
@@ -15,40 +15,54 @@ namespace MOHPDClient
     {
         private readonly string TEAMHOOFDWEGENRP = "TeamHoofdWegenRP";
         private readonly string MELDING = "Melding";
-        private readonly string GIERIGENARROGANT = "Gierig en arrogant";
-        
+        private readonly string GIERIGENARROGANT = "Gul en Vredelievend";
+
+        private bool blipEnabled = false;
+        private int blip;
+
         private String config = Function.Call<String>(Hash.LOAD_RESOURCE_FILE, "MOHPD", "config.ini");
         private ArrayList vehicleModels;
         private ArrayList vehicles;
-        
-        Menu menu = new Menu("TeamHoofdwegenRP","Gierig en Arrogant");
-        MenuItem inmelden = new MenuItem("Inmelden","Gierig en Arrogant");
-        MenuItem discord = new MenuItem("Discord", "Link naar de TeamHoofdwegenRP Discord.");
-        MenuItem calloutsMenuButton = new MenuItem("Meldingen", "Hier kunnen meldingen worden gegenereerd.") {Label = "→→→"};
-        MenuItem voertuigenMenuButton = new MenuItem("Voertuigen", "Hier kan je een voertuig inspawnen.") {Label = "→→→"};
 
-        Menu calloutsMenu = new Menu("Meldingen", "Gierig en Arrogant");
+        Menu menu = new Menu("TeamHoofdwegenRP", "Gul en Vredelievend");
+        MenuItem inmelden = new MenuItem("Inmelden");
+        MenuItem discord = new MenuItem("Discord", "Link naar de TeamHoofdwegenRP Discord.");
+
+        MenuItem calloutsMenuButton = new MenuItem("Meldingen", "Hier kunnen meldingen worden gegenereerd.")
+            {Label = "→→→"};
+
+        MenuItem voertuigenMenuButton = new MenuItem("Voertuigen", "Hier kan je een voertuig inspawnen.")
+            {Label = "→→→"};
+
+        MenuItem noodknop = new MenuItem("",
+                "Alleen gebruiken in geval van nood, stuurt een signaal naar alle collega's!")
+            {LeftIcon = MenuItem.Icon.WARNING, Label = "~r~Noodknop"};
+
+
+        Menu calloutsMenu = new Menu("Meldingen", "Gul en Vredelievend");
         MenuItem startVTB = new MenuItem("VTB");
-        
-        Menu voertuigenMenu = new Menu("Voertuigen", "Gierig en Arrogant");
+
+        Menu voertuigenMenu = new Menu("Voertuigen", "Gul en Vredelievend");
 
 
         public MOHPDClient()
         {
             EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
             EventHandlers["CL:Inmelden"] += new Action<string>(Inmelden);
-            EventHandlers["CL:Notify"] += new Action<string,string, string>(Notify);
+            EventHandlers["CL:Notify"] += new Action<string, string, string>(Notify);
             EventHandlers["CL:PlaySound"] += new Action<string, double>(playSound);
+            EventHandlers["CL:NoodknopGPS"] += new Action<Vector3>(NoodknopGPS);
+
 
             vehicleModels = new ArrayList();
             vehicles = new ArrayList();
             LoadConfig();
-            
+
             MenuController.MenuToggleKey = Control.SelectCharacterMichael;
             MenuController.AddMenu(menu);
             MenuController.AddSubmenu(menu, calloutsMenu);
             MenuController.AddSubmenu(menu, voertuigenMenu);
-            
+
             menu.AddMenuItem(inmelden);
             menu.AddMenuItem(discord);
 
@@ -56,22 +70,28 @@ namespace MOHPDClient
             {
                 if (item == inmelden)
                 {
-                    TriggerServerEvent("SV:Inmelden", GetPlayerPed(-1));
+                    TriggerServerEvent("SV:Inmelden");
                     TriggerEvent("CL:Inmelden", GetPlayerPed(-1));
                 }
+
                 if (item == discord)
                 {
-                    TriggerServerEvent("SV:Discord", GetPlayerPed(-1));
+                    TriggerServerEvent("SV:Discord");
+                }
+
+                if (item == noodknop)
+                {
+                    TriggerServerEvent("SV:NoodknopIngedrukt", Game.Player.Character.Position);
                 }
             };
-            
+
             calloutsMenu.AddMenuItem(startVTB);
-            
+
             calloutsMenu.OnItemSelect += (menu, item, index) =>
             {
                 if (item == startVTB)
                 {
-                    TriggerServerEvent("SV:VTB", GetPlayerPed(-1));
+                    TriggerServerEvent("SV:VTB");
                 }
             };
 
@@ -79,7 +99,7 @@ namespace MOHPDClient
             {
                 voertuigenMenu.AddMenuItem(car);
             }
-            
+
             voertuigenMenu.OnItemSelect += (menu, item, index) =>
             {
                 foreach (MenuItem m in vehicles)
@@ -90,7 +110,7 @@ namespace MOHPDClient
                     }
                 }
             };
-            
+
             MenuController.MenuAlignment = MenuController.MenuAlignmentOption.Right;
             Tick += OnTick;
         }
@@ -99,26 +119,26 @@ namespace MOHPDClient
         {
             StringReader strReader = new StringReader(config);
             String configLine = strReader.ReadLine();
-            if(configLine != null && configLine == "<vehicles>")
+            if (configLine != null && configLine == "<vehicles>")
             {
-                while(true)
+                while (true)
                 {
                     configLine = strReader.ReadLine();
-                    if(configLine == "</vehicles>")
+                    if (configLine == "</vehicles>")
                     {
                         return;
                     }
+
                     String[] strlist = configLine.Split(':');
                     vehicleModels.Add(strlist[1]);
                     vehicles.Add(new MenuItem(strlist[0]));
                 }
             }
         }
-        
+
         private void OnClientResourceStart(string resourceName)
         {
             if (GetCurrentResourceName() != resourceName) return;
-
         }
 
         private async Task OnTick()
@@ -127,6 +147,16 @@ namespace MOHPDClient
             {
                 TriggerServerEvent("SV:FluitjePls");
             }
+
+            if (blipEnabled)
+            {
+                if (Game.Player.Character.Position.DistanceToSquared(GetBlipCoords(blip)) < 3000)
+                {
+                    SetBlipRoute(blip, false);
+                    RemoveBlip(ref blip);
+                }
+            }
+
             await Delay(100);
         }
 
@@ -135,7 +165,7 @@ namespace MOHPDClient
             var hash = (uint) GetHashKey(model);
             if (!IsModelInCdimage(hash) || !IsModelAVehicle(hash))
             {
-                Notify("Oeps! Dit voertuig bestaat helaas niet!",TEAMHOOFDWEGENRP,GIERIGENARROGANT);
+                Notify("Oeps! Dit voertuig bestaat helaas niet!", TEAMHOOFDWEGENRP, GIERIGENARROGANT);
                 return;
             }
 
@@ -146,30 +176,39 @@ namespace MOHPDClient
             }
             catch (Exception e)
             {
-                
             }
+
             var vehicle = await World.CreateVehicle(model, Game.PlayerPed.Position, Game.PlayerPed.Heading);
-    
+
             // set the player ped into the vehicle and driver seat
             Game.PlayerPed.SetIntoVehicle(vehicle, VehicleSeat.Driver);
         }
-        
+
         private void Inmelden(string resourceName)
         {
             menu.RemoveMenuItem(inmelden);
             menu.RemoveMenuItem(discord);
             menu.AddMenuItem(calloutsMenuButton);
             MenuController.BindMenuItem(menu, calloutsMenu, calloutsMenuButton);
-            
+
             menu.AddMenuItem(voertuigenMenuButton);
             MenuController.BindMenuItem(menu, voertuigenMenu, voertuigenMenuButton);
-            
+
             menu.AddMenuItem(discord);
-            
-            RegisterCommand("stcallout", new Action<int, List<object>, string>((source, args, raw) =>
-            {
-                TriggerServerEvent("SV:Callout", GetPlayerFromServerId(source));
-            }), false);
+            menu.AddMenuItem(noodknop);
+
+            RegisterCommand("stcallout",
+                new Action<int, List<object>, string>((source, args, raw) =>
+                {
+                    TriggerServerEvent("SV:Callout", GetPlayerFromServerId(source));
+                }), false);
+
+            RegisterCommand("noodknop",
+                new Action<int, List<object>, string>((source, args, raw) =>
+                {
+                    TriggerServerEvent("SV:GetNoodknopIngedrukt", DateTime.UtcNow.ToString());
+                    Notify("De route is toegevoegd aan je GPS!", "Assistentie Collega",TEAMHOOFDWEGENRP);
+                }), false);
         }
 
         private void Notify(string message, string title, string subtitle)
@@ -182,7 +221,16 @@ namespace MOHPDClient
 
         private void playSound(string file, double vol)
         {
-            TriggerEvent("Client:SoundToClient",  file, vol);
+            TriggerEvent("Client:SoundToClient", file, vol);
+        }
+
+        private void NoodknopGPS(Vector3 v)
+        {
+            blip = AddBlipForCoord(v.X, v.Y, v.Z);
+            SetBlipColour(blip, 58);
+            SetBlipRouteColour(blip, 58);
+            SetBlipRoute(blip, true);
+            blipEnabled = true;
         }
     }
 }
--- MOHPDServer/MOHPDServer.cs
+++ MOHPDServer/MOHPDServer.cs
@@ -14,13 +14,16 @@ namespace MOHPDServer
     {
         private readonly string TEAMHOOFDWEGENRP = "TeamHoofdWegenRP";
         private readonly string MELDING = "Melding";
-        private readonly string GIERIGENARROGANT = "Gierig en arrogant";
+        private readonly string GIERIGENARROGANT = "Gul en Vredelievend";
         private readonly string MELDING_VTB = "Melding: VTB";
-        
-        
+
+
         private static PoliceDAO policeDao = new PoliceDAO();
         public static int[] dispatchColors = new[] {52, 113, 235};
         private static string dispatchText = "[Meldkamer POL]";
+        private Vector3 mostRecentNoodknop;
+        private DateTime mostRecentNoodknopTime;
+
         public MOHPDServer()
         {
             EventHandlers["SV:Inmelden"] += new Action<Player>(SvInmelden);
@@ -28,35 +31,39 @@ namespace MOHPDServer
             EventHandlers["SV:VTB"] += new Action<Player>(SvVTB);
             EventHandlers["SV:FluitjePls"] += new Action<Player>(SvFluitjePls);
             EventHandlers["SV:Discord"] += new Action<Player>(SvDiscord);
+            EventHandlers["SV:NoodknopIngedrukt"] += new Action<Player, Vector3>(SvNoodknopIngedrukt);
+            EventHandlers["SV:GetNoodknopIngedrukt"] += new Action<Player, string>(SvGetNoodknopIngedrukt);
             EventHandlers["playerDropped"] += new Action<Player, string>(OnPlayerDropped);
             Tick += OnTick;
         }
 
         private void SvFluitjePls([FromSource] Player player)
         {
-            TriggerEvent("Server:SoundToRadius", player.Character.NetworkId , 50.0f, "fluit", 0.5f);
+            TriggerEvent("Server:SoundToRadius", player.Character.NetworkId, 50.0f, "fluit", 0.5f);
         }
 
         private void SvInmelden([FromSource] Player player)
         {
             if (policeDao.Contains(player))
             {
-                player.TriggerEvent("CL:Notify","U bent reeds ingemeld!",$"~r~{TEAMHOOFDWEGENRP}",GIERIGENARROGANT);
+                player.TriggerEvent("CL:Notify", "U bent reeds ingemeld!", $"~r~{TEAMHOOFDWEGENRP}", GIERIGENARROGANT);
             }
             else
             {
                 policeDao.Add(player);
-                player.TriggerEvent("CL:Notify","U bent succesvol ingemeld, fijne dienst!",$"~b~{TEAMHOOFDWEGENRP}",GIERIGENARROGANT);
+                player.TriggerEvent("CL:Notify", "U bent succesvol ingemeld, fijne dienst!", $"~b~{TEAMHOOFDWEGENRP}",
+                    GIERIGENARROGANT);
             }
         }
-        
+
         private void SvCallout([FromSource] Player source)
         {
             foreach (var player in policeDao)
             {
-                player.TriggerEvent("CL:Notify","je kanker moeder",$"~r~{TEAMHOOFDWEGENRP}",GIERIGENARROGANT);
+                player.TriggerEvent("CL:Notify", "je kanker moeder", $"~r~{TEAMHOOFDWEGENRP}", GIERIGENARROGANT);
             }
         }
+
         private async Task OnTick()
         {
             await Delay(100);
@@ -67,19 +74,56 @@ namespace MOHPDServer
             Callout newCallout = new VTB();
             foreach (var player in policeDao)
             {
-                player.TriggerEvent("CL:Notify",newCallout.GetCalloutNotification(),MELDING_VTB,TEAMHOOFDWEGENRP);
-                player.TriggerEvent("CL:PlaySound","meldingVTB", 1.0f);
+                player.TriggerEvent("CL:Notify", newCallout.GetCalloutNotification(), MELDING_VTB, TEAMHOOFDWEGENRP);
+                player.TriggerEvent("CL:PlaySound", "meldingVTB", 1.0f);
             }
         }
-        
+
         private void SvDiscord([FromSource] Player source)
         {
-            source.TriggerEvent("CL:Notify","~b~Discord: ~w~https://discord.gg/WTBWBNv",$"~b~{TEAMHOOFDWEGENRP}",GIERIGENARROGANT);
+            source.TriggerEvent("CL:Notify", "~b~Discord: ~w~https://discord.gg/WTBWBNv", $"~b~{TEAMHOOFDWEGENRP}",
+                GIERIGENARROGANT);
         }
-        
-        private void OnPlayerDropped([FromSource]Player player, string reason)
+
+        private void SvNoodknopIngedrukt([FromSource] Player source, Vector3 pos)
         {
-            if(policeDao.Contains(player)) policeDao.Remove(player);
+            mostRecentNoodknop = pos;
+            mostRecentNoodknopTime = DateTime.UtcNow;
+            foreach (var player in policeDao)
+            {
+                if (!player.Equals(source))
+                {
+                    player.TriggerEvent("CL:PlaySound", "panicButton", 1.0f);
+                    player.TriggerEvent("CL:Notify",
+                        "~r~Noodknop ingedrukt~n~~w~Gebruik /noodknop om de gps in te stellen!",
+                        "Assistentie Collega", TEAMHOOFDWEGENRP);
+                }
+            }
+
+            source.TriggerEvent("CL:PlaySound", "panicButton", 1.0f);
+            source.TriggerEvent("CL:Notify",
+                "~r~Noodknop ingedrukt~n~~w~De collega's zijn gealarmeerd naar jouw locatie!",
+                "Assistentie Collega", TEAMHOOFDWEGENRP);
+        }
+
+        private void SvGetNoodknopIngedrukt([FromSource] Player source, string d)
+        {
+            if (mostRecentNoodknop != null && mostRecentNoodknopTime != null)
+            {
+                TimeSpan diff = Convert.ToDateTime(d).Subtract(mostRecentNoodknopTime);
+                if (diff.TotalSeconds <= 90)
+                {
+                    source.TriggerEvent("CL:NoodknopGPS", mostRecentNoodknop);
+                    return;
+                }
+            }
+            source.TriggerEvent("CL:Notify", "Er is momenteel geen noodknop actief!", "Assistentie Collega",
+                    TEAMHOOFDWEGENRP);
+        }
+
+        private void OnPlayerDropped([FromSource] Player player, string reason)
+        {
+            if (policeDao.Contains(player)) policeDao.Remove(player);
         }
     }
 }
